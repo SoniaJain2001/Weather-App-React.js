f027646927fea46999af05326582aa6e0f6a8126
Sonia Jain <soniajain2001@gmail.com>
2023-01-31 22:14:12 +0530

    First commit

---
+++ react_sonia/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Restaurant from './component/basics/Restaurant';
+// import UseState from './component/Hooks/useState';
+// import UseEffect from './component/Hooks/useEffect';
+// import UseReducer from './component/Hooks/useReducer';
+// import Todo from './component/todoreact.js/todo';
+import Temp from './component/weather/temp';
+const App = () => {
+  return (
+    
+      <Temp/>
+   
+  )
+  
+};
+
+export default App
